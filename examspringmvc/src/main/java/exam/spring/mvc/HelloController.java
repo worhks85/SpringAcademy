632ce1cf1e17dd4ec41a7ac395797c283542f7f1
Jaekwan Kim <worhks5017@naver.com>
2022-06-27 19:07:59 +0900

    6/27 Practice

---
+++ examspringmvc/src/main/java/exam/spring/mvc/HelloController.java
@@ -0,0 +1,15 @@
+package exam.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+	
+	
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "hello";
+	}
+}
